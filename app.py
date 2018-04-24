be31390e2819495a678531f7af967aaa719790ec
YuriNU <30548760+YuriNU@users.noreply.github.com>
2018-04-24 13:41:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,12 @@ def echo(bot, update):
     update.message.reply_text(reference_text)
     update.message.reply_text(refrence_url)
     update.message.reply_text(json_data)
-
+    
+    url_srsearch='https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch='+update.message.text+'&srwhat=text&continue='
+    response = requests.get(url_srsearch)
+    json_data = json.loads(response.text)
+    update.message.reply_text(json_data["query"]["search"][0]["pageid"])
+    update.message.reply_text(json_data["query"]["search"][0]["title"])
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"' % (update, error))
 
